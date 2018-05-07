50e5ebdbc58b5d0537cb6d920f2fc70e35c0e05a
Andrew Landis <andrew.landis3@gmail.com>
2018-05-07 18:30:54 -0400

    new file: smart_mirror.py

---
+++ smart_mirror.py
@@ -0,0 +1,25 @@
+# Script to control updating the display
+from tkinter import *
+from settings import Settings
+from main_panel import Main
+import time
+
+
+# Creates window
+root = Tk()
+root.title("Weather App")
+root.configure(background = 'black')
+
+# Establish settings variable
+settings = Settings()
+
+# Create panel class with settings
+Main = Main(settings)
+
+# Call panel function to create itself
+Main.create(root)
+root.mainloop()
+
+
+
+
